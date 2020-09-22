90eb7564259dbbd407fb4bf0316fecfe5d08f52f
Mou Torabinejad <mou@NB-WXE15395.tib.tibub.de>
2020-09-22 12:44:40 +0200

    remove all in order to add APP folder

--- data_generator/source/run_generator.py
+++
@@ -1,3 +0,0 @@
-from generator.generate_data import run_generation
-import sys
-run_generation(number_of_datasets=int(sys.argv[1]),output_path=str(sys.argv[2]))
--- source/run_normalization.py
+++
@@ -1,4 +0,0 @@
-from normalization.normalize import normalize
-import sys
-normalize()
-#normalize(config_path=str(sys.argv[1]),csv_output_path=str(sys.argv[2]),mapping_output_path=str(sys.argv[3]))
